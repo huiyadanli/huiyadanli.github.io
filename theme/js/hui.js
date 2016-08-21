344a230abd3e2e30ab1a3941facd2fb573020ec4
huiyadanli <huiyadanli@gmail.com>
2016-08-21 12:58:26 +0800

    fix bug and edit some detail

--- theme/js/hui.js
+++ theme/js/hui.js
@@ -9,78 +9,12 @@
  
 // huiyadanli new add
 
-
-
 // set theme path, got from http://stackoverflow.com/questions/8523200/javascript-get-current-filescript-path
 (function() {
     var scriptEls = document.getElementsByTagName('script');
     var thisScriptEl = scriptEls[scriptEls.length - 1];
     var scriptPath = thisScriptEl.src;
     window.gThemePath = scriptPath.substr(0, scriptPath.lastIndexOf('/js/'));
-    
-    // dynamically modify title
-    jQuery(document).ready(function() {
-        function c() {
-            document.title = document[a] ? "标题被我吃掉了 (°∀°)ﾉ - " + d : d
-        }
-        var a, b, d = document.title;
-        "undefined" != typeof document.hidden ? (a = "hidden", b = "visibilitychange") : "undefined" != typeof document.mozHidden ? (a = "mozHidden", b = "mozvisibilitychange") : "undefined" != typeof document.webkitHidden && (a = "webkitHidden", b = "webkitvisibilitychange");
-        "undefined" == typeof document.addEventListener && "undefined" == typeof document[a] || document.addEventListener(b, c, !1)
-    });
-
-    // mouse click special effects
-    var click_cnt=0;
-    jQuery(document).ready(function($) {
-        $("html").click(function(e) {
-            var n=18;
-            //var $i=$("<b></b>").text("+"+n);
-            var $i;
-            click_cnt++;
-            if(click_cnt==10) {
-                $i=$("<b></b>").text("OωO");
-            } else if(click_cnt==20) {
-                $i=$("<b></b>").text("(๑•́ ∀ •̀๑)");
-            } else if(click_cnt==30) {
-                $i=$("<b></b>").text("(๑•́ ₃ •̀๑)");
-            } else if(click_cnt==40) {
-                $i=$("<b></b>").text("(๑•̀_•́๑)");
-            } else if(click_cnt==50) {
-                $i=$("<b></b>").text("（￣へ￣）");
-            } else if(click_cnt==60) {
-                $i=$("<b></b>").text("(╯°口°)╯(┴—┴");
-            } else if(click_cnt==70) {
-                $i=$("<b></b>").text("૮( ᵒ̌皿ᵒ̌ )ა");
-            } else if(click_cnt==80) {
-                $i=$("<b></b>").text("╮(｡>口<｡)╭");
-            } else if(click_cnt==90) {
-                $i=$("<b></b>").text("( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃");
-            } else if(click_cnt>=100 && click_cnt<=105) {
-                $i=$("<b></b>").text("(ꐦ°᷄д°᷅)");
-            } else {
-                $i=$("<i class='fa fa-heart'></i>");
-                n=Math.round(Math.random()*14+6)
-            }
-            var x=e.pageX,y=e.pageY;
-            $i.css({
-                "z-index":9999,
-                "top":y-20,
-                "left":x,
-                "position":"absolute",
-                "color":"#E94F06",
-                "font-size":n,
-                "-moz-user-select": "none",
-                "-webkit-user-select": "none",
-                "-ms-user-select": "none"
-            });
-            $("body").append($i);
-            $i.animate(
-                {"top":y-180,"opacity":0},
-                1500,
-                function(){$i.remove();}
-            );
-            //e.stopPropagation();
-        });
-    });
 
     // headroom.js -> navbar
     var myElement = document.querySelector(".navbar-fixed-top");
@@ -95,6 +29,9 @@
        }
     });
     headroom.init(); 
+
+    // output
+    console.log("(。﹏。) 不要这么看人家，人家害羞\nCopyright © 2015-2016 Made with ♥ by huiyadanli");
 
     // canvas special effects from http://evanyou.me/
     /*
@@ -147,6 +84,73 @@
 
 
 })();
+
+// dynamically modify title
+jQuery(document).ready(function() {
+    function c() {
+        document.title = document[a] ? "标题被我吃掉了 (°∀°)ﾉ - " + d : d
+    }
+    var a, b, d = document.title;
+    "undefined" != typeof document.hidden ? (a = "hidden", b = "visibilitychange") : "undefined" != typeof document.mozHidden ? (a = "mozHidden", b = "mozvisibilitychange") : "undefined" != typeof document.webkitHidden && (a = "webkitHidden", b = "webkitvisibilitychange");
+    "undefined" == typeof document.addEventListener && "undefined" == typeof document[a] || document.addEventListener(b, c, !1)
+});
+
+// mouse click special effects
+var click_cnt=0;
+jQuery(document).ready(function($) {
+    $("html").click(function(e) {
+        var n=18;
+        //var $i=$("<b></b>").text("+"+n);
+        var $i;
+        click_cnt++;
+        if(click_cnt==10) {
+            $i=$("<b></b>").text("OωO");
+        } else if(click_cnt==20) {
+            $i=$("<b></b>").text("(๑•́ ∀ •̀๑)");
+        } else if(click_cnt==30) {
+            $i=$("<b></b>").text("(๑•́ ₃ •̀๑)");
+        } else if(click_cnt==40) {
+            $i=$("<b></b>").text("(๑•̀_•́๑)");
+        } else if(click_cnt==50) {
+            $i=$("<b></b>").text("（￣へ￣）");
+        } else if(click_cnt==60) {
+            $i=$("<b></b>").text("(╯°口°)╯(┴—┴");
+        } else if(click_cnt==70) {
+            $i=$("<b></b>").text("૮( ᵒ̌皿ᵒ̌ )ა");
+        } else if(click_cnt==80) {
+            $i=$("<b></b>").text("╮(｡>口<｡)╭");
+        } else if(click_cnt==90) {
+            $i=$("<b></b>").text("( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃");
+            // Fuzzy magic!
+            $("p").css("color", "transparent");
+            $("p").css("text-shadow", "#111 0 0 5px");
+        } else if(click_cnt>=100 && click_cnt<=105) {
+            $i=$("<b></b>").text("(ꐦ°᷄д°᷅)");
+        } else {
+            $i=$("<i class='fa fa-heart'></i>");
+            n=Math.round(Math.random()*14+6)
+        }
+        var x=e.pageX,y=e.pageY;
+        $i.css({
+            "z-index":9999,
+            "top":y-20,
+            "left":x,
+            "position":"absolute",
+            "color":"#E94F06",
+            "font-size":n,
+            "-moz-user-select": "none",
+            "-webkit-user-select": "none",
+            "-ms-user-select": "none"
+        });
+        $("body").append($i);
+        $i.animate(
+            {"top":y-180,"opacity":0},
+            1500,
+            function(){$i.remove();}
+        );
+        //e.stopPropagation();
+    });
+});
 
 
 // --------
