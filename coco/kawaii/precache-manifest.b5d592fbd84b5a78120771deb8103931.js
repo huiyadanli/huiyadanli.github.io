425a6663fc70b6217d195cadd57ed08c965d1da1
huiyadanli <huiyadanli@gmail.com>
2019-08-12 00:37:53 +0800

    fix path

---
+++ coco/kawaii/precache-manifest.b5d592fbd84b5a78120771deb8103931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c17f6ff193f5150fe8e307c35192906",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9aaf22a9d44b8c99518",
+    "url": "./static/css/main.948314f5.chunk.css"
+  },
+  {
+    "revision": "a3544e3efd4d7cc01ad3",
+    "url": "./static/js/2.4104ef7a.chunk.js"
+  },
+  {
+    "revision": "a9aaf22a9d44b8c99518",
+    "url": "./static/js/main.d91af93c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8668cb0b87bd485effc0669164f6eff5",
+    "url": "./static/media/coconut.8668cb0b.png"
+  }
+]);
--- coco/kawaii/service-worker.js
+++ coco/kawaii/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ab9a93242c56f1d2bd46669fce2cd8e.js"
+  "./precache-manifest.b5d592fbd84b5a78120771deb8103931.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
